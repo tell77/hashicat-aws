0aaadff6166ddcaf96a1d0c2f5253802699dcd93
Karl Mayer <karl.mayer@lilly.com>
2019-10-24 17:52:05 +0000

    Foo!

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_key_pair" "hashicat" {
 
 resource "aws_vpc" "hashicat" {
   cidr_block           = "${var.address_space}"
-  enable_dns_hostnames = false
+  enable_dns_hostnames = true
 
   tags = {
     Name = "${var.prefix}-vpc"
