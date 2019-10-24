07b6b8fe68016ffebe60e81dee4e35d5890fdb21
tell77 <elliott_trevor@lilly.com>
2019-10-24 17:16:51 +0000

    Updated website.

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -8,11 +8,11 @@ cat << EOM > /var/www/html/index.html
   <body>
   <div style="width:800px;margin: 0 auto">
 
-  <!-- BEGIN -->
+ 
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
-  <!-- END -->
+  Welcome to ${PREFIX}'s app. I need a nap right meow. 
+ 
   
   </div>
   </body>
